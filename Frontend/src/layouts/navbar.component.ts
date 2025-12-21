fb5e0b9e85ad958cc267441f6f577626d8e88bb9
JOSUE <joshvm1235@gmail.com>
2025-12-21 00:40:15 -0600

    navbar con logo en src/assets

---
+++ Frontend/src/layouts/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component, signal } from '@angular/core';
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+}
