69dbe0dbbaf705411add015a53154cbeaaff71b3
Katia Jaqueline <katiaucdzib@gmail.com>
2025-12-22 22:48:12 -0600

    diseño de la pagina Mis Pedidos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ProductDescriptionComponent } from './pages/users_pages/product-description/product-description';  
 import { CartComponent } from './pages/users_pages/cart/cart';
+import { OrdersComponent } from './pages/users_pages/orders/orders';
 export const routes: Routes = [
   {
     path: '',
     component: ProductDescriptionComponent
-  }, {path: 'cart', component: CartComponent}
+  }, {path: 'cart', component: CartComponent},
+  {path: 'orders', component: OrdersComponent}
 ];
---
+++ Frontend/src/app/pages/users_pages/orders/orders.ts
@@ -0,0 +1,63 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-orders',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './orders.html',
+  styleUrls: ['./orders.css']
+})
+export class OrdersComponent {
+
+  selectedFilter: string = 'Todos';
+
+  orders = [
+    {
+      id: 'ORD-7829',
+      date: '24 Oct, 2023',
+      status: 'Entregado',
+      items: [
+        'assets/product_description/moños.png',
+        'assets/product_description/gio.png'
+      ],
+      extra: 2,
+      total: 124.50,
+      action: 'Ver detalles'
+    },
+    {
+      id: 'ORD-7830',
+      date: '02 Nov, 2023',
+      status: 'En proceso',
+      items: [
+        'assets/product_description/garnier.png'
+      ],
+      total: 45.00,
+      action: 'Rastrear pedido'
+    },
+    {
+      id: 'ORD-7812',
+      date: '15 Oct, 2023',
+      status: 'Enviado',
+      items: [
+        'assets/product_description/miss-dior.png',
+        'assets/product_description/gio.png'
+      ],
+      total: 280.00,
+      action: 'Rastrear paquete'
+    }
+  ];
+
+  get filteredOrders() {
+    if (this.selectedFilter === 'Todos') {
+      return this.orders;
+    }
+    return this.orders.filter(
+      order => order.status === this.selectedFilter
+    );
+  }
+
+  setFilter(filter: string) {
+    this.selectedFilter = filter;
+  }
+}
