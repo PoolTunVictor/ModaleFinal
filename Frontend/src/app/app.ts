9fe86bd700e54408c9a21a8c16562e4580f797ef
DanielAlberto013 <danielhidalgo013@gmail.com>
2025-12-26 21:13:49 -0600

    Fix app.ts merge conflicts and router imports

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,36 +1,27 @@
 import { Component, signal } from '@angular/core';
-<<<<<<< HEAD
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../layouts/navbar.component';
-import { Footer } from './layouts/footer/footer';
-=======
-import { Route, RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../layouts/navbar/navbar.component';
-import { UserSidebarComponent } from '../layouts/user_sidebar/user-sidebar.component';
-import { Router } from '@angular/router';
->>>>>>> login
+import { Footer } from './layouts/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
   imports: [
     RouterOutlet,
     NavbarComponent,
     Footer
   ],
-=======
-  imports: [RouterOutlet, NavbarComponent,UserSidebarComponent],
->>>>>>> login
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('ModAle');
 
-  constructor (private router: Router){}
-  get showLayout(): boolean{
-    const currentUrl = this.router.url;
-    return !(currentUrl.includes('/login') || currentUrl.includes('/register'));
+  protected readonly title = signal('ModAle');
+  showLayout = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.showLayout = !this.router.url.includes('login');
+    });
   }
 }
