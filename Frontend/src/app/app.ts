301d22de168b2787c5847db342e4561da1dc2713
DanielAlberto013 <danielhidalgo013@gmail.com>
2025-12-26 21:53:59 -0600

    victor gei

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../layouts/navbar/navbar.component';
 import { Footer } from './layouts/footer/footer';
+import { UserSidebarComponent } from '../layouts/user_sidebar/user-sidebar.component';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +10,8 @@ import { Footer } from './layouts/footer/footer';
   imports: [
     RouterOutlet,
     NavbarComponent,
-    Footer
+    Footer,
+    UserSidebarComponent
   ],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
@@ -17,11 +19,31 @@ import { Footer } from './layouts/footer/footer';
 export class App {
 
   protected readonly title = signal('ModAle');
-  showLayout = true;
+
+  // Estados de layout
+  showLayout = true;        // navbar
+  showUserSidebar = false; // sidebar usuario
+  showFooter = true;       // footer
 
   constructor(private router: Router) {
     this.router.events.subscribe(() => {
-      this.showLayout = !this.router.url.includes('login');
+      const url = this.router.url;
+
+      // ❌ Login: nada de layout
+      this.showLayout = !url.includes('login');
+
+      // ✅ Sidebar SOLO en "Mi cuenta"
+      this.showUserSidebar = url.startsWith('/perfil');
+
+      // ❌ Footer NO en admin
+      const isAdminRoute =
+        url.startsWith('/inicio') ||
+        url.startsWith('/add_product') ||
+        url.startsWith('/inventory') ||
+        url.startsWith('/reports') ||
+        url.startsWith('/users');
+
+      this.showFooter = !isAdminRoute && this.showLayout;
     });
   }
 }
