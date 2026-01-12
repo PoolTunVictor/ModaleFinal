f620257190760112205c991830d6f69a123505dc
DanielAlberto013 <danielhidalgo013@gmail.com>
2026-01-12 12:19:43 -0600

    nose

--- Frontend/src/app/pages/admin_pages/reports/reports.ts
+++ Frontend/src/app/pages/admin_pages/reports/reports.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminReportService } from '../../../core/service/admin-report.service';
-import { Chart, registerables } from 'chart.js';
-
-Chart.register(...registerables);
 
 @Component({
   selector: 'app-reports',
@@ -16,15 +13,11 @@ export class Reports implements OnInit {
 
   report: any = null;
   isLoading = true;
-  salesChart: any;
 
-  constructor(
-    private adminReportService: AdminReportService
-  ) {}
+  constructor(private adminReportService: AdminReportService) {}
 
   ngOnInit(): void {
     this.loadReport();
-    this.loadSalesChart();
   }
 
   loadReport() {
@@ -40,47 +33,4 @@ export class Reports implements OnInit {
       }
     });
   }
-
-  loadSalesChart() {
-    this.adminReportService.getSalesByDay().subscribe(data => {
-
-      const labels = data.map(d => d.date);
-      const totals = data.map(d => d.total);
-
-      const ctx = document.getElementById('salesChart') as HTMLCanvasElement;
-
-      if (this.salesChart) {
-        this.salesChart.destroy();
-      }
-
-      this.salesChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels,
-          datasets: [
-            {
-              label: 'Ventas ($)',
-              data: totals,
-              borderColor: '#22c55e',
-              backgroundColor: 'rgba(34,197,94,0.2)',
-              tension: 0.3,
-              fill: true
-            }
-          ]
-        },
-        options: {
-          responsive: true,
-          plugins: {
-            legend: { display: false }
-          },
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
-    });
-  }
-
 }
