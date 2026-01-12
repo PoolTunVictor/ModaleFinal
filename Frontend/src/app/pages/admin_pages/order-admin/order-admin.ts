32d5277a1c06f1e1ac72ec59ee30f74bdbe31e6f
DanielAlberto013 <danielhidalgo013@gmail.com>
2026-01-12 05:37:50 -0600

    Agregacion de order admin

---
+++ Frontend/src/app/pages/admin_pages/order-admin/order-admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-admin',
+  imports: [],
+  templateUrl: './order-admin.html',
+  styleUrl: './order-admin.css',
+})
+export class OrderAdmin {
+
+}
