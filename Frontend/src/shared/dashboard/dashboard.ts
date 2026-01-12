9ccdd13abfbcc05dc48c9008537efd7d066cbbe9
DanielAlberto013 <danielhidalgo013@gmail.com>
2026-01-12 00:03:16 -0600

    Obtencion de usuario logueado en el dashboard

--- Frontend/src/shared/dashboard/dashboard.ts
+++ Frontend/src/shared/dashboard/dashboard.ts
@@ -1,28 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../app/core/service/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
+
+  user: any = null;
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    this.user = this.authService.getUser();
+  }
+
   logout() {
     this.authService.logout();
-
-    // 🔐 limpiar cualquier redirect pendiente
     localStorage.removeItem('redirectAfterLogin');
-
-    // 👉 mandar al login
     this.router.navigate(['/login']);
   }
 }
