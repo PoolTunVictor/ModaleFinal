32e32132426166e798c933fb632b6f6112d1034a
DanielAlberto013 <danielhidalgo013@gmail.com>
2026-01-12 21:56:21 -0600

    Funcion de reportes

--- Frontend/src/app/pages/admin_pages/reports/reports.ts
+++ Frontend/src/app/pages/admin_pages/reports/reports.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AdminReportService } from '../../../core/service/admin-report.service';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './reports.html',
   styleUrl: './reports.css'
 })
@@ -13,6 +14,9 @@ export class Reports implements OnInit {
 
   report: any = null;
   isLoading = true;
+
+  tableMode: 'top' | 'least' | 'all' = 'top';
+  displayedProducts: any[] = [];
 
   constructor(private adminReportService: AdminReportService) {}
 
@@ -26,6 +30,7 @@ export class Reports implements OnInit {
     this.adminReportService.getSummary().subscribe({
       next: (data) => {
         this.report = data;
+        this.applyTableMode();
         this.isLoading = false;
       },
       error: () => {
@@ -33,4 +38,31 @@ export class Reports implements OnInit {
       }
     });
   }
+
+  applyTableMode() {
+    if (!this.report || !this.report.products) return;
+
+    const products = [...this.report.products];
+
+    // TOP: 20 unidades o más
+    if (this.tableMode === 'top') {
+      this.displayedProducts = products
+        .filter(p => p.units >= 20)
+        .sort((a, b) => b.units - a.units);
+    }
+
+    // MENOS VENDIDOS: 5 unidades o menos
+    if (this.tableMode === 'least') {
+      this.displayedProducts = products
+        .filter(p => p.units <= 5)
+        .sort((a, b) => a.units - b.units);
+    }
+
+    // TODOS
+    if (this.tableMode === 'all') {
+      this.displayedProducts = products
+        .sort((a, b) => b.units - a.units);
+    }
+  }
+
 }
